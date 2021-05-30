41a776c2ab4f01f2b61aadb4ce540d3c7a423072
brkss <berkassebrahim@gmail.com>
2021-05-30 19:59:58 +0100

    🥑 setup graphql

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Application } from './Application';
 import { ChakraProvider } from "@chakra-ui/react"
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/react-hooks';
 
+const client : any = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache({})
+}) 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Application />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Application />
+      </ChakraProvider>
+    </ApolloProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useHelloQuery } from '../generated/graphql';
 
 
 export const HomePage : React.FC = () => {
 
+    const {loading, data} = useHelloQuery();
+
+    if(loading) return <>loading...</>
     return(
         <>
-            Home Page
+            {data?.hello}
         </>
     );
 }
