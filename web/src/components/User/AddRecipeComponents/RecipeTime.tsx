3d0523bdf58a2164bb3e55a1ee654ae649077963
brkss <berkassebrahim@gmail.com>
2021-11-03 01:36:53 +0100

    🔧 update time UI (scrollable slide with no scrollbar )

--- web/src/components/User/AddRecipeComponents/RecipeTime.tsx
+++ web/src/components/User/AddRecipeComponents/RecipeTime.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { Box, Text, Center, Wrap, WrapItem } from "@chakra-ui/react";
 
-const times = ["Breakfast", "Lunch", "Dinner"];
+const times = ["Breakfast", "Lunch", "Dinner", "Snack"];
 
 export const RecipeTime: React.FC = () => {
   return (
     <Box>
-      <Text fontWeight={"bold"}>Time</Text>
+      <Text fontWeight={"bold"}>Type</Text>
       <Box mt={"5px"}>
-        <Wrap>
+        <ul
+          className={"options"}
+          style={{ whiteSpace: "nowrap", overflow: "scroll" }}
+        >
           {times.map((time, key) => (
-            <WrapItem key={key}>
+            <li
+              key={key}
+              style={{ display: "inline-block", marginRight: "15px" }}
+            >
               <Center bg={"#EDEBEB"} rounded={7} p={"10px"} minW={"150px"}>
                 <Text fontWeight={"bold"}>{time}</Text>
               </Center>
-            </WrapItem>
+            </li>
           ))}
-        </Wrap>
+        </ul>
       </Box>
     </Box>
   );
