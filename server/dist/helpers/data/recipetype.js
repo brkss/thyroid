ad8f0b8b77413eb62326b05767df8a6a0c06544a
brkss <berkassebrahim@gmail.com>
2021-11-09 23:03:30 +0100

    💿 Recipe Types Data

---
+++ server/dist/helpers/data/recipetype.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.types = void 0;
+exports.types = [
+    "Atkins",
+    "The Zone",
+    "Ketogenic",
+    "Vegetarian",
+    "Vegan",
+    "Weight Watchers",
+    "South Beach",
+    "Raw food",
+    "Mediterranean",
+];
+//# sourceMappingURL=recipetype.js.map
---
+++ server/src/helpers/data/recipetype.ts
@@ -0,0 +1,11 @@
+export const types = [
+  "Atkins",
+  "The Zone",
+  "Ketogenic",
+  "Vegetarian",
+  "Vegan",
+  "Weight Watchers",
+  "South Beach",
+  "Raw food",
+  "Mediterranean",
+];
