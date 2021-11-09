20b35b977d29ab8a5af145164e970f763409f3b0
brkss <berkassebrahim@gmail.com>
2021-11-09 19:39:34 +0100

    chore: Add recipe choose type !

--- web/src/components/User/AddRecipeComponents/RecipeType.tsx
+++ web/src/components/User/AddRecipeComponents/RecipeType.tsx
@@ -4,6 +4,24 @@ import { Box, Text, Center, Wrap, WrapItem } from "@chakra-ui/react";
 const types = ["AUP", "LC", "Gluten Free", "KETO"];
 
 export const RecipeType: React.FC = () => {
+  const [selected, SetSelected] = React.useState<string[]>([]);
+
+  const selectElement = (type: string) => {
+    if (!selected.includes(type)) {
+      selected.push(type);
+      SetSelected(selected);
+    } else {
+      const index = selected.findIndex((x) => x == type);
+      const tmp = selected;
+      tmp.splice(index, 1);
+      SetSelected(tmp);
+    }
+  };
+  const is_selected = (type: string) => {
+    if (selected.includes(type)) return true;
+    return false;
+  };
+
   return (
     <Box>
       <Text fontWeight={"bold"}>Type</Text>
@@ -14,8 +32,13 @@ export const RecipeType: React.FC = () => {
         >
           {types.map((type, key) => (
             <li
+              onClick={() => selectElement(type)}
               key={key}
-              style={{ display: "inline-block", marginRight: "15px" }}
+              style={{
+                display: "inline-block",
+                marginRight: "15px",
+                background: is_selected(type) ? "red" : "",
+              }}
             >
               <Center bg={"#EDEBEB"} rounded={7} p={"10px"} minW={"150px"}>
                 <Text fontWeight={"bold"}>{type}</Text>
